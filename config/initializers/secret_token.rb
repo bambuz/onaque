2a15571bdfddaa6ab136ff9213f27870c4a01f8d
bam <peto@bam.sk>
2014-03-04 16:27:42 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Onaque::Application.config.secret_key_base = '34b7503df01e0b1bc8137b20dd8db425193f9a5d93eebdd2c0a192d312f22e27754f2164c52cbfec53efaca99140b29b8f326d4acc79587a1d13b7a27b2b4932'
